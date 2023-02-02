f7060d3f9d1f515c448ffbfbea8b5c2a0701335b
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-02-02 18:16:45 +0900

    23修正

--- public/js/hikuyousya_regist.js
+++ public/js/hikuyousya_regist.js
@@ -1,3 +1,50 @@
+window.addEventListener('DOMContentLoaded', function(){
+  
+    // input要素を取得
+    var meinichi = document.getElementById("meinichi");
+  
+    // イベントリスナーでイベント「change」を登録
+    meinichi.addEventListener("change",function(){
+
+        temp_date = this.value;
+
+        const birthday = {
+            year: temp_date.substr(0,4),
+            month: temp_date.substr(5,2),
+            date:  temp_date.substr(8,2)
+        };
+
+        console.log(birthday);
+        //今日
+        var today = new Date();
+    
+        //今年の誕生日
+        var thisYearsBirthday = new Date(today.getFullYear(), birthday.month-1, birthday.date);
+    
+        //年齢
+        var age = today.getFullYear() - birthday.year;
+
+        if(today < thisYearsBirthday){
+            //今年まだ誕生日が来ていない
+            age--;
+        }
+
+        if (age == 0) {
+            age = 1;
+        } else {
+            age = age + 2;
+        }
+
+        console.log(age);
+        let kaiki_year = document.getElementById('kaiki_year');
+        kaiki_year.value = age;
+
+    });
+  
+});
+
+
+
 function clickTextStoreButton() {
     error_flg = 0;
 
