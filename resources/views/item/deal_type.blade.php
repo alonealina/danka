6aa8243daef85209b5e28d68ecb6c4a09abb853b
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-02-02 20:08:12 +0900

    取引一覧修正

--- resources/views/item/deal_type.blade.php
+++ resources/views/item/deal_type.blade.php
@@ -3,6 +3,17 @@
     <div class="payment_btn_div">すべて</div>
     @else
     <a href="{{ route('deal_list', [
+            'number' => '10',
+            'name' => $name,
+            'name_kana' => $name_kana,
+            'tel' => $tel,
+            'item_category_id' => $item_category_id,
+            'created_at_before' => $created_at_before,
+            'created_at_after' => $created_at_after,
+            'payment_before' => $payment_before,
+            'payment_after' => $payment_after,
+            'price_min' => $price_min,
+            'price_max' => $price_max,
             'type' => 'すべて',
             ]) }}"
             class="payment_btn_a">すべて</a>
@@ -12,6 +23,17 @@
     <div class="payment_btn_div" style="border-left: 1px solid;">送付待ち</div>
     @else
     <a href="{{ route('deal_list', [
+        'number' => '10',
+            'name' => $name,
+            'name_kana' => $name_kana,
+            'tel' => $tel,
+            'item_category_id' => $item_category_id,
+            'created_at_before' => $created_at_before,
+            'created_at_after' => $created_at_after,
+            'payment_before' => $payment_before,
+            'payment_after' => $payment_after,
+            'price_min' => $price_min,
+            'price_max' => $price_max,
             'type' => '送付待ち',
             ]) }}"
             class="payment_btn_a" style="border-left: 1px solid;">送付待ち</a>
@@ -21,6 +43,17 @@
     <div class="payment_btn_div" style="border-left: 1px solid;border-right: 1px solid;">未払い</div>
     @else
     <a href="{{ route('deal_list', [
+        'number' => '10',
+            'name' => $name,
+            'name_kana' => $name_kana,
+            'tel' => $tel,
+            'item_category_id' => $item_category_id,
+            'created_at_before' => $created_at_before,
+            'created_at_after' => $created_at_after,
+            'payment_before' => $payment_before,
+            'payment_after' => $payment_after,
+            'price_min' => $price_min,
+            'price_max' => $price_max,
             'type' => '未払い',
             ]) }}"
             class="payment_btn_a" style="border-left: 1px solid;border-right: 1px solid;">未払い</a>
@@ -30,6 +63,17 @@
         <div class="payment_btn_div">支払済</div>
     @else
     <a href="{{ route('deal_list', [
+        'number' => '10',
+            'name' => $name,
+            'name_kana' => $name_kana,
+            'tel' => $tel,
+            'item_category_id' => $item_category_id,
+            'created_at_before' => $created_at_before,
+            'created_at_after' => $created_at_after,
+            'payment_before' => $payment_before,
+            'payment_after' => $payment_after,
+            'price_min' => $price_min,
+            'price_max' => $price_max,
             'type' => '支払済',
             ]) }}"
             class="payment_btn_a">支払済</a>
