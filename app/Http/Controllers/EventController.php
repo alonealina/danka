12c31e56810a6e142a73f6e49a2502bf06b9c9f4
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-02-08 20:35:57 +0900

    行事新規追加時の検索画面の修正

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -386,11 +386,9 @@ class EventController extends Controller
                     ->leftJoin('item', 'item.id', '=', 'deal_detail.item_id');
                 if (!empty($payment_before)) {
                     $query_tmp->whereDate('payment_date', '>=', $payment_before);
-                    $query->whereDate('payment_date', '>=', $payment_before);
                 }
                 if (!empty($payment_after)) {
                     $query_tmp->whereDate('payment_date', '<=', $payment_after);
-                    $query->whereDate('payment_date', '<=', $payment_after);
                 }
                 $query_tmp->where('item.category_id', 3);
                 $not_danka_ids = array_unique($query_tmp->get()->pluck('id')->toArray());
